44db1aa784b8a127c875c3a0add7c25290007ad6
vlash <shupliakovvl@gmail.com>
2023-12-16 19:01:23 +0200

    [fp][lab2] update lab2 for convenient cabal structure

--- lab2/app/ParallelDijkstra.hs
+++ lab2/app/ParallelDijkstra.hs
@@ -1,12 +1,12 @@
-module ParallelDijkstra where
+module Main where
 
 import Control.Concurrent
 import Control.Monad
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
 import System.Clock
+import Data.List (minimumBy, delete)
+import Data.Ord (comparing)
 
 type Node = Int
 type Weight = Int
@@ -14,18 +14,20 @@ type Edge = (Node, Weight)
 type Graph = Map Node [Edge]
 
 dijkstra :: Graph -> Node -> Map Node Weight
-dijkstra graph start = dijkstra' initialDistances (Set.singleton start)
+dijkstra graph start = dijkstra' initialDistances [start]
   where
     initialDistances = Map.fromList [(node, if node == start then 0 else maxBound) | node <- Map.keys graph]
 
     dijkstra' distances unvisited
-      | Set.null unvisited = distances
-      | otherwise = let (nearest, dist) = Set.findMin unvisited in
-                      dijkstra' (updateDistances nearest dist distances) (Set.delete nearest unvisited)
+      | null unvisited = distances
+      | otherwise =
+          let filteredDistances = [(node, dist) | (node, dist) <- Map.toList distances, node `elem` unvisited]
+              (nearest, nearestDist) = minimumBy (comparing snd) filteredDistances
+          in dijkstra' (updateDistances nearest nearestDist distances) (delete nearest unvisited)
 
     updateDistances node dist distances = foldl (updateDistance node dist) distances (Map.findWithDefault [] node graph)
 
-    updateDistance srcNode srcDist distances (destNode, weight) =
+    updateDistance _ srcDist distances (destNode, weight) =
       let alt = srcDist + weight in
       Map.adjust (min alt) destNode distances
 
@@ -34,7 +36,7 @@ parallelDijkstra :: Graph -> [Node] -> IO [Map Node Weight]
 parallelDijkstra graph starts = do
     mvars <- forM starts $ \start -> do
         mvar <- newEmptyMVar
-        forkIO $ do
+        _ <- forkIO $ do
             let minPath = dijkstra graph start
             putMVar mvar minPath
         return mvar
@@ -51,4 +53,3 @@ main = do
 
     mapM_ print results
     putStrLn $ "Elapsed time: " ++ show elapsedTime ++ " seconds"
-
