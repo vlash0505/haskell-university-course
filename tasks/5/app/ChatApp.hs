806d3d243d412084b872374599500d808d57d4e7
vlash <shupliakovvl@gmail.com>
2023-12-17 11:30:37 +0200

    [fp] fix client handling ing the task 5 for the chat app

--- tasks/5/app/ChatApp.hs
+++ tasks/5/app/ChatApp.hs
@@ -6,11 +6,12 @@ import qualified Network.Socket.ByteString as NSB
 import Graphics.UI.Threepenny.Core
 import Network.Socket
 import Control.Concurrent (forkIO)
+import Control.Concurrent (threadDelay)
 import Control.Concurrent
 import Control.Concurrent.STM
 import System.IO
 import Control.Monad (forever, unless, forM_)
-import Control.Exception (finally, bracket, catch, handle, IOException)
+import Control.Exception (finally, bracket, try, catch, handle, IOException)
 
 type Client = (Socket, TChan String)
 type ClientList = TVar [Client]
@@ -29,6 +30,15 @@ startServer = withSocketsDo $ do
         (conn, _) <- accept sock
         forkIO $ runConn conn clients
 
+setupConnection :: String -> String -> (Socket -> IO ()) -> IO ()
+setupConnection host port action = withSocketsDo $ do
+    addrinfos <- getAddrInfo Nothing (Just host) (Just port)
+    let serveraddr = head addrinfos
+    sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+    connect sock (addrAddress serveraddr)
+    action sock
+    close sock
+
 runConn :: Socket -> ClientList -> IO ()
 runConn conn clients = do
     chan <- atomically newTChan
@@ -41,11 +51,13 @@ clientHandler conn clients = handleClient conn `catch` clientDisconnected
   where
     handleClient sock = do
         msg <- NSB.recv sock 1024
-        unless (C.null msg) $ do
-            let receivedMsg = C.unpack msg
-            putStrLn $ "Received message: " ++ receivedMsg
-            broadcast receivedMsg clients
-            handleClient sock
+        if C.null msg
+            then removeClient sock clients  -- Client disconnected, remove it
+            else do
+                let receivedMsg = C.unpack msg
+                putStrLn $ "Received message: " ++ receivedMsg
+                broadcast receivedMsg clients
+                handleClient sock
 
     clientDisconnected :: IOException -> IO ()
     clientDisconnected e = do
@@ -54,31 +66,29 @@ clientHandler conn clients = handleClient conn `catch` clientDisconnected
 
 removeClient :: Socket -> ClientList -> IO ()
 removeClient conn clients = do
-    putStrLn "Removing client"  -- Log client removal
+    putStrLn "Removing client"
     atomically $ modifyTVar' clients $ filter ((/= conn) . fst)
+    close conn  -- Close the socket
 
 broadcast :: String -> ClientList -> IO ()
 broadcast msg clients = do
     clist <- atomically $ readTVar clients
-    forM_ clist $ \(sock, _) -> NSB.send sock (C.pack msg) `catch` handleSendException sock
+    forM_ clist $ \(sock, _) -> try (NSB.send sock (C.pack msg)) >>= handleResult sock
   where
-    handleSendException :: Socket -> IOException -> IO Int
-    handleSendException sock e = do
-        putStrLn $ "Broadcast exception for client: " ++ show sock ++ ", error: " ++ show e
-        return 0  -- Return a default value indicating error
+    handleResult :: Socket -> Either IOException Int -> IO ()
+    handleResult sock (Left e) = do
+        putStrLn $ "Error sending to client: " ++ show sock ++ ", error: " ++ show e
+        removeClient sock clients
+    handleResult _ (Right _) = return ()  -- Successfully sent, do nothing
 
-sendMsg :: String -> IO ()
-sendMsg msg = withSocketsDo $ do
-    putStrLn $ "Sending message: " ++ msg  -- Debug print
-    addrinfos <- getAddrInfo Nothing (Just "localhost") (Just "3000")
-    let serveraddr = head addrinfos
-    sock <- socket (addrFamily serveraddr) Stream defaultProtocol
-    connect sock (addrAddress serveraddr)
-    NSB.send sock (C.pack msg)
-    close sock
+sendMsg :: Socket -> String -> IO ()
+sendMsg sock msg = do
+    putStrLn $ "Sending message: " ++ msg
+    _ <- NSB.send sock (C.pack msg)  -- Discard the return value
+    return ()
 
-setup :: Window -> UI ()
-setup window = do
+setup :: Socket -> Window -> UI ()
+setup sock window = do
     return window # set title "Haskell Chat"
 
     -- UI elements
@@ -92,12 +102,16 @@ setup window = do
     -- Event handling
     on UI.click sendButton $ \_ -> do
         message <- get value input
-        liftIO $ sendMsg message  -- Send message to server
+        liftIO $ sendMsg sock message
         newMsg <- UI.div #+ [string ("You: " ++ message)]
         element output #+ [element newMsg]
         element input # set value ""
 
 main :: IO ()
 main = do
-    forkIO startServer  -- Start the server in a separate thread
-    startGUI defaultConfig setup  -- Start the GUI
+    forkIO startServer
+    threadDelay 1000000  -- Wait for 1 second for the server to start
+    let host = "localhost"
+    let port = "3000"
+    setupConnection host port $ \sock ->
+        startGUI defaultConfig (setup sock)
